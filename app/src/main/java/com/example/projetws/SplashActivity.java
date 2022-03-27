731029b27ceb77391b9b659afee49b9fe334bb8c
yassine bissaoui <yassine.bissaoui@gmail.com>
2022-03-27 12:19:23 +0000

    splash Screeen

--- app/src/main/java/com/example/projetws/SplashActivity.java
+++ app/src/main/java/com/example/projetws/SplashActivity.java
@@ -90,7 +90,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
-        },500);
+        },4000);
     }
 
     protected void onPause() {
